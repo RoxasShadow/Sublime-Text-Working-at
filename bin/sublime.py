aca214f79698e20d97c83ca65a5ff3bd801ad155
Giovanni Capuano <webmaster@giovannicapuano.net>
2013-07-31 00:13:44 +0200

    Error handling

--- bin/sublime.py
+++ bin/sublime.py
@@ -8,12 +8,13 @@ __module_description__ = "Say in chan at what you're working for"
 import xchat as XC
 
 def sublime(word, word_eol, userdata):
-	p = sub.Popen(os.getcwd() + "\\plugins\\sublime.exe", stdout=sub.PIPE, stderr=sub.PIPE)
-	output, errors = p.communicate()
-	if output == "Error":
-		XC.command("me " + "is working at nothing.")
-		XC.command("me " + "is working at " + output.split("- Sublime Text")[0].split("\\")[-1] + ".")
-	
+  p = sub.Popen(os.getenv('APPDATA') + "\\HexChat\\addons\\sublime.exe", stdout=sub.PIPE, stderr=sub.PIPE)
+  output, errors = p.communicate()
+  if output == "Error":
+    XC.command("me " + "is working at nothing.")
+  else:
+   XC.command("me " + "is working at " + output.split("- Sublime Text")[0].split("\\")[-1].strip() + ".")
+
 XC.hook_command("sublime", sublime, help="/sublime Say in chan at what you're working for")
 
 XC.prnt(__module_name__ + " version " + __module_version__ + " loaded.")
